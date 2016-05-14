d73c7483cbbbd01717d3c8321f624875d575b01b
Checkwhei Sin <Checkwhei Sin>
2016-05-14 11:01:10 -0400

    radio button

--- Checkbox/app/src/main/java/com/example/jacka/checkbox/MainActivity.java
+++ Checkbox/app/src/main/java/com/example/jacka/checkbox/MainActivity.java
@@ -6,20 +6,54 @@ import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CheckBox checkBoxDog, checkBoxCat, checkBoxCow;
     private Button selectCheckBoxs;
+    private CheckBox checkBoxDog, checkBoxCat, checkBoxCow;
+
+    private static RadioGroup radioGroup;
+    private static Button selectRadioButton;
+    private static RadioButton radioButton1, radioButton2,radioButton3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnSelectCheckBoxs();
+
+        onClickListenerForRadioButtons();
     }
 
+
+    // ----------------- Radio button ------------------------------
+    public void onClickListenerForRadioButtons() {
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        selectRadioButton = (Button) findViewById(R.id.radioSelectButton);
+
+        selectRadioButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButtonSelected = (RadioButton) findViewById(selectedId);
+                Toast.makeText(MainActivity.this, "Selected Radio Button " + radioButtonSelected.
+                        getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+
+    // ------------------- Check boxes --------------------------------
+    /**
+     * Add Listeners to the other selected check boxes
+     */
     public void addListenerOnSelectCheckBoxs() {
         checkBoxCat = (CheckBox) findViewById(R.id.checkBoxCat);
         checkBoxCow = (CheckBox) findViewById(R.id.checkBoxCow);
