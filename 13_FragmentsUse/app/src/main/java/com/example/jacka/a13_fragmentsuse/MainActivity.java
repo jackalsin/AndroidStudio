5417effb99131a76a1fa4488bd1caaf5cb0c7f15
Checkwhei Sin <Checkwhei Sin>
2016-05-15 19:12:36 -0400

    set up the 13_Fragment

---
+++ 13_FragmentsUse/app/src/main/java/com/example/jacka/a13_fragmentsuse/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jacka.a13_fragmentsuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
